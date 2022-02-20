b2e9bd4afbbcfdb8642e82afe4e8f0fea8007e0a
Franck Blettner <franck.blettner@nomana-it.fr>
2022-02-20 21:53:02 +0100

    Update License Header

--- src/nomabc/BarcodeUtil.java
+++ src/nomabc/BarcodeUtil.java
@@ -1,12 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
+ * Copyright (c) 2018 NOMANA-IT and/or its affiliates.
+ * All rights reserved. Use is subject to license terms.
  *
- * @author franckblettner
+ * @author fblettner
  */
 package nomabc;
 
